36aa0890fe7298c54f0ef4e1d77bee13183d0996
ícaro <icarosancho@gmail.com>
2026-08-01 04:03:16 -0300

    chore: add own output handler

--- main.cpp
+++ main.cpp
@@ -3,11 +3,25 @@
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
+#include <cstdio>
+#include <QtGlobal>
+
+void myMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    Q_UNUSED(context);
+    fprintf(stderr, "[QT] %s\n", qPrintable(msg));
+    fflush(stderr);
+}
 
 int main(int argc, char *argv[])
 {
+    printf("BEFORE HANDLER INSTALL\n");
+    fflush(stdout);
+    qInstallMessageHandler(myMessageHandler);
+    qDebug() << "HANDLER TEST";
+    printf("AFTER QDEBUG CALL\n");
+    fflush(stdout);
     QApplication a(argc, argv);
-
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for (const QString &locale : uiLanguages) {
@@ -19,7 +33,6 @@ int main(int argc, char *argv[])
     }
     QApplication::setOrganizationName("misha");
     QApplication::setApplicationName("AAQT");
-
     MainWindow w;
     w.show();
     return QApplication::exec();
